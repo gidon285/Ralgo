c5145efa28372101ddd9096abeffc6781553eba3
gidon <gidonaviz1@gmail.com>
2022-04-06 11:10:53 +0300

    added tests ex1

--- HM4/ex1/ex1.py
+++ HM4/ex1/ex1.py
@@ -1,14 +1,69 @@
 
 import unittest
 
-def bounded_subset(lst: list, num: int):
-    if not isinstance(lst, list) or not isinstance(num, int):
-        raise ValueError("This generator accepts only (list, int) input")
-    temp = [ x for x in lst if x <= num].sort()
-    ans = []
-    for i in range(len(lst)):
-        if i == 0 :
-            ans.append([])
-        ans.extend(subs for subs in [subset + [i] for subset in ans] if sum(subs) <= num)
+def bounded_subset(lst: list, n: int):
+    if not isinstance(lst, list) or not isinstance(n, int):
+        raise Exception("wrong input")
+    temp = sorted([ x for x in lst if x <= n])
+    ans = [[]]
+    for num in lst:
+        ans.extend(val for val in [a + [num] for a in ans] if sum(val) <= n)
     return ans
 
+def check_if_contains_list(lst1: list, lst2: list) -> bool:
+    for subset in lst2:
+        if all(x in subset for x in lst1):
+            return True
+    return False
+
+
+class tests(unittest.TestCase):
+
+    def test_subsets(self):
+        lst1 = [ 5, 2 ,6 ,10, 1]
+        ans1 = bounded_subset(lst1, 10)
+        self.assertTrue(ans1.__contains__([]))
+        self.assertTrue(ans1.__contains__([2,6]))
+        self.assertTrue(not ans1.__contains__([16]))
+
+
+        lst2 = [ 5, 6 ,7 ,8, 9]
+        ans2 = bounded_subset(lst1, 11)
+        self.assertTrue(ans2.__contains__([]))
+        self.assertTrue(ans2.__contains__([5,6]))
+        self.assertTrue(not ans2.__contains__([16]))
+
+    def test_bad_inputs(self):
+        n = 5
+        obj1= {"a":"1","b":"2"}
+        obj2= {"c":"3","d":"4"}
+        obj3= ({"a":"2","b":"3"})
+        obj4= ("a","2","b","3")
+        obj5= (10 ,9, 8, 7)
+        try:
+            check = bounded_subset(obj1, n)
+        except Exception:
+            pass
+        try:
+            check = bounded_subset(obj2, n)
+        except Exception:
+            pass
+        try:
+            check = bounded_subset(obj3, n)
+        except Exception:
+            pass
+        try:
+            check = bounded_subset(obj4, n)
+        except Exception:
+            pass
+        try:
+            check = bounded_subset(obj1, n)
+        except Exception:
+            pass
+        try:
+            check = bounded_subset(obj5, n)
+        except Exception:
+            self.assertEqual(True,True)
+
+if __name__ == "__main__":
+    unittest.main()
